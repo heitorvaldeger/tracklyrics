4bce9715a6af27b737a4fbdde2117fc18f7861be
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-10-15 21:56:47 -0300

    test: ensure VideoController returns 400 if isDraft is not a boolean

--- backend/tests/unit/controllers/VideoController.spec.ts
+++ backend/tests/unit/controllers/VideoController.spec.ts
@@ -2,7 +2,8 @@ import { randomUUID } from 'node:crypto'
 import VideoController from '#controllers/VideoController'
 import Video from '#models/video'
 import { test } from '@japa/runner'
-import { ok } from '../../../app/helpers/http.js'
+import { badRequest, ok } from '../../../app/helpers/http.js'
+import { HttpContextFactory } from '@adonisjs/core/factories/http'
 
 test.group('VideoController', (group) => {
   group.teardown(async () => {
@@ -24,5 +25,31 @@ test.group('VideoController', (group) => {
     const videos = await sut.findAll()
 
     assert.deepEqual(videos, ok([fakeVideo]))
+  })
+
+  test('should returns 400 if isDraft is not a boolean', async ({ assert }) => {
+    const fakeVideo = {
+      isDraft: 'any_value',
+      title: 'any_title',
+      artist: 'any_artist',
+      releaseYear: '2000',
+      linkYoutube: 'any_link',
+    }
+
+    const httpContext = new HttpContextFactory().create()
+    httpContext.request.updateBody(fakeVideo)
+    const sut = new VideoController()
+    const httpResponse = await sut.create(httpContext)
+
+    assert.deepEqual(
+      httpResponse,
+      badRequest([
+        {
+          field: 'isDraft',
+          message: 'The value must be a boolean',
+          rule: 'boolean',
+        },
+      ])
+    )
   })
 })
