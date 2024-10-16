00070ccaf79f20bb73c5b4150e7a7ee783979eb4
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-10-15 21:24:36 -0300

    test: add field uuid to into Video model

--- backend/tests/unit/controllers/VideoController.spec.ts
+++ backend/tests/unit/controllers/VideoController.spec.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'node:crypto'
 import VideoController from '#controllers/VideoController'
 import Video from '#models/video'
 import { test } from '@japa/runner'
@@ -8,17 +9,20 @@ test.group('VideoController', (group) => {
     await Video.query().whereNotNull('id').delete()
   })
   test('should returns 200 if a list videos returns on success', async ({ assert }) => {
-    const video = await Video.create({
+    const uuid = randomUUID()
+    const fakeVideo = {
       isDraft: false,
       title: 'any_title',
       artist: 'any_artist',
       qtyViews: BigInt(0),
       releaseYear: '2000',
       linkYoutube: 'any_link',
-    })
+      uuid: uuid,
+    }
+    await Video.create(fakeVideo)
     const sut = new VideoController()
     const videos = await sut.findAll()
 
-    assert.deepEqual(videos, ok([video.serialize()]))
+    assert.deepEqual(videos, ok([fakeVideo]))
   })
 })
