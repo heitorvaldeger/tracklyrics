4534d191728cf5d7dd2b9cbf7a373fb7484c949f
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-10-19 00:24:02 -0300

    test: ensure VideoController returns 200 if a video return on success

--- backend/tests/unit/controllers/VideoController.spec.ts
+++ backend/tests/unit/controllers/VideoController.spec.ts
@@ -45,21 +45,24 @@ test.group('VideoController', (group) => {
     sinon.reset()
   })
   test('should returns 200 if a list videos returns on success', async ({ assert }) => {
-    const uuid = randomUUID()
-    const fakeVideo = {
-      isDraft: false,
-      title: 'any_title',
-      artist: 'any_artist',
-      qtyViews: BigInt(0),
-      releaseYear: '2000',
-      linkYoutube: 'any_link',
-      uuid: uuid,
-    }
-    await Video.create(fakeVideo)
+    const fakeVideo = await makeFakeVideo()
     const sut = new VideoController()
     const videos = await sut.findAll()
 
     assert.deepEqual(videos, ok([fakeVideo]))
+  })
+
+  test('should returns 200 if a video return on success', async ({ assert }) => {
+    const fakeVideo = await makeFakeVideo()
+    const httpContext = new HttpContextFactory().create()
+    stub(httpContext.request, 'params').returns({
+      uuid: fakeVideo.uuid,
+    })
+
+    const sut = new VideoController()
+    const video = await sut.find(httpContext)
+
+    assert.deepEqual(video, ok(fakeVideo))
   })
 
   test('should returns 400 if isDraft is not a boolean', async ({ assert }) => {
