6b9cc6ad7c610c61fef3423bec787ca8087a9b7c
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-10-24 22:45:58 -0300

    test: updates

---
+++ backend/tests/unit/controllers/video/video-controller-findByGenrer.spec.ts
@@ -0,0 +1,35 @@
+import _ from 'lodash'
+import { test } from '@japa/runner'
+import VideoController from '#controllers/VideoController'
+import Video from '#models/video'
+import sinon, { stub } from 'sinon'
+import { ok } from '#helpers/http'
+import { makeFakeVideo } from '#tests/factories/makeFakeVideo'
+import { makeFakeVideoServiceStub } from '#tests/factories/makeFakeVideoServiceStub'
+import { HttpContextFactory } from '@adonisjs/core/factories/http'
+
+const makeSut = async () => {
+  const { fakeVideo, language, genrer } = await makeFakeVideo()
+
+  const { videoServiceStub, videoStub } = makeFakeVideoServiceStub(fakeVideo, language, genrer)
+  const sut = new VideoController(videoServiceStub)
+
+  return { sut, videoStub, videoServiceStub }
+}
+
+test.group('VideoController.findByGenrer', (group) => {
+  group.setup(async () => {
+    await Video.query().whereNotNull('id').delete()
+  })
+
+  test('should returns 200 if a list videos returns on success', async ({ expect }) => {
+    const httpContext = new HttpContextFactory().create()
+    stub(httpContext.request, 'params').returns({
+      genrerId: 0,
+    })
+    const { videoStub, sut } = await makeSut()
+    const httpResponse = await sut.findByGenrer(httpContext)
+
+    expect(httpResponse).toEqual(ok([videoStub]))
+  })
+})
