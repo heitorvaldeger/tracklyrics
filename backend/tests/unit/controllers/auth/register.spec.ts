e18072af5aa6de74661fde600d89fd40f45c4be6
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-10-29 21:59:55 -0300

    test: updates

---
+++ backend/tests/unit/controllers/auth/register.spec.ts
@@ -0,0 +1,103 @@
+import { test } from '@japa/runner'
+import { stub } from 'sinon'
+import { badRequest, ok } from '#helpers/http'
+import AuthController from '#controllers/AuthController'
+import { makeHttpRequestBody } from '#tests/factories/makeHttpRequestBody'
+
+const makeFakeRequest = () => ({
+  username: 'any_username',
+  email: 'any_mail@mail.com',
+  password: 'any_password',
+  firstName: 'any_firstName',
+  lastName: 'any_lastName',
+})
+const makeSut = () => {
+  const httpContext = makeHttpRequestBody(makeFakeRequest())
+
+  const sut = new AuthController()
+
+  return { sut, httpContext }
+}
+test.group('AuthController.register', () => {
+  test('should returns 400 if required fields is not provided', async ({ expect }) => {
+    const { sut, httpContext } = makeSut()
+
+    stub(httpContext.request, 'body').returns({})
+    const httpResponse = await sut.register(httpContext)
+
+    expect(httpResponse).toEqual(
+      badRequest([
+        {
+          field: 'email',
+          message: 'The email field must be defined',
+        },
+        {
+          field: 'password',
+          message: 'The password field must be defined',
+        },
+        {
+          field: 'username',
+          message: 'The username field must be defined',
+        },
+        {
+          field: 'firstName',
+          message: 'The firstName field must be defined',
+        },
+        {
+          field: 'lastName',
+          message: 'The lastName field must be defined',
+        },
+      ])
+    )
+  })
+
+  test('should returns 400 if email provided is invalid', async ({ expect }) => {
+    const { sut, httpContext } = makeSut()
+
+    stub(httpContext.request.body(), 'email').value('invalid_mail')
+    const httpResponse = await sut.register(httpContext)
+
+    expect(httpResponse).toEqual(
+      badRequest([
+        {
+          field: 'email',
+          message: 'The email field must be a valid email address',
+        },
+      ])
+    )
+  })
+
+  test('should returns 400 if fields provided is less than length valid', async ({ expect }) => {
+    const { sut, httpContext } = makeSut()
+
+    stub(httpContext.request, 'body').returns({
+      email: 'valid_mail@mail.com',
+      password: 'any',
+      username: 'any',
+      firstName: '',
+      lastName: '',
+    })
+    const httpResponse = await sut.register(httpContext)
+
+    expect(httpResponse).toEqual(
+      badRequest([
+        {
+          field: 'password',
+          message: 'The password field must have at least 6 characters',
+        },
+        {
+          field: 'username',
+          message: 'The username field must have at least 4 characters',
+        },
+        {
+          field: 'firstName',
+          message: 'The firstName field must have at least 1 characters',
+        },
+        {
+          field: 'lastName',
+          message: 'The lastName field must have at least 1 characters',
+        },
+      ])
+    )
+  })
+})
