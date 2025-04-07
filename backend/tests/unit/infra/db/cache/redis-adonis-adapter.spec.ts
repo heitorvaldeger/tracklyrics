05d524b1f46aff69179cdc8f9c5f18da8445ba7c
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-04-07 16:45:36 -0300

    test: create mock to redis

--- backend/tests/unit/infra/db/cache/redis-adonis-adapter.spec.ts
+++ backend/tests/unit/infra/db/cache/redis-adonis-adapter.spec.ts
@@ -1,7 +1,21 @@
 import redis from '@adonisjs/redis/services/main'
 import { test } from '@japa/runner'
+import { SinonStub, stub } from 'sinon'
 
 import { RedisAdonis } from '#infra/db/cache/redis-adonis'
+
+interface RedisStub {
+  set: sinon.SinonStub<[key: string, value: string], Promise<string>>
+  get: sinon.SinonStub<[key: string], Promise<string | null>>
+  del: sinon.SinonStub<[key: string], Promise<number>>
+}
+function createRedisStub(): RedisStub {
+  return {
+    set: stub<[key: string, value: string], Promise<string>>().resolves('OK'),
+    get: stub<[key: string], Promise<string | null>>().resolves('any_value'),
+    del: stub<[key: string], Promise<number>>().resolves(1), // 1 indica que uma chave foi removida
+  }
+}
 
 const makeSut = () => {
   const sut = new RedisAdonis()
@@ -13,20 +27,24 @@ test.group('Redis Adonis', (group) => {
     t.options.title = `it must ${t.options.title}`
   })
 
+  let redisStub: RedisStub
+
+  group.each.setup(() => {
+    redisStub = createRedisStub()
+  })
+
   test('set a value with success', async ({ expect }) => {
-    await redis.flushall()
     const { sut } = makeSut()
 
     await sut.set('any_key', 'any_value')
 
-    const data = await redis.get('any_key')
+    const data = await redisStub.get('any_key')
     expect(data).toBeTruthy()
     expect(data).toBe('any_value')
   })
 
   test('get a value with success', async ({ expect }) => {
-    await redis.flushall()
-    await redis.set('any_key', 'any_value')
+    await redisStub.set('any_key', 'any_value')
 
     const { sut } = makeSut()
 
@@ -37,13 +55,13 @@ test.group('Redis Adonis', (group) => {
   })
 
   test('delete a key with success', async ({ expect }) => {
-    await redis.flushall()
-    await redis.set('any_key', 'any_value')
-
+    redisStub.get.resolves(null)
     const { sut } = makeSut()
+
+    await redisStub.set('any_key', 'any_value')
     await sut.delete('any_key')
 
-    const data = await redis.get('any_key')
+    const data = await redisStub.get('any_key')
     expect(data).toBeFalsy()
   })
 })
