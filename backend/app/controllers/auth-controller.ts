2857e6be7b0214ef4bd3ccde6ff3f7f4a65ee1d2
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-05-29 17:28:10 -0300

    refactor: remove unnecessary method

--- backend/app/controllers/auth-controller.ts
+++ backend/app/controllers/auth-controller.ts
@@ -2,7 +2,6 @@ import { inject } from '@adonisjs/core'
 import { HttpContext } from '@adonisjs/core/http'
 
 import { IAuthService } from '#services/interfaces/auth-service'
-import { validateEmailValidator } from '#validators/vinejs/auth-validator'
 
 @inject()
 export default class AuthController {
@@ -11,13 +10,5 @@ export default class AuthController {
   async logout({ response }: HttpContext) {
     await this.authService.logout()
     return response.noContent()
-  }
-
-  async validateEmail({ request, response }: HttpContext) {
-    const [errors, data] = await validateEmailValidator.tryValidate(request.body())
-    if (errors || !data) {
-      return response.badRequest(errors.messages)
-    }
-    return await this.authService.validateEmail(data)
   }
 }
