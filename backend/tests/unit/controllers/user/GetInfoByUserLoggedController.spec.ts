74aa9a6539989f02dd0b15f9827a95d134de0406
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-05-30 22:33:40 -0300

    test: move user controller to an unique file

--- backend/tests/unit/controllers/user/GetInfoByUserLoggedController.spec.ts
+++ backend/tests/unit/controllers/user/GetInfoByUserLoggedController.spec.ts
@@ -1,54 +1,54 @@
 import { test } from '@japa/runner'
 import { stub } from 'sinon'
 
-import UserController from '#controllers/user-controller'
+import GetInfoByUserLoggedController from '#controllers/user/GetInfoByUserLoggedController'
 import UnauthorizedException from '#exceptions/unauthorized-exception'
 import UserNotFoundException from '#exceptions/user-not-found-exception'
 import { mockUserService, mockUserWithoutPasswordData } from '#tests/__mocks__/stubs/mock-user-stub'
 
 const makeSut = () => {
-  const sut = new UserController(mockUserService)
+  const sut = new GetInfoByUserLoggedController(mockUserService)
 
   return { sut }
 }
 
-test.group('UserController', (group) => {
+test.group('User/GetInfoByUserLoggedController', (group) => {
   group.tap((t) => {
     t.options.title = `it must ${t.options.title}`
   })
 
-  test('return 200 with user information on success', async ({ expect }) => {
+  test('return success with user information on success', async ({ expect }) => {
     const { sut } = makeSut()
 
-    const httpResponse = await sut.getFullInfoByUserLogged()
+    const httpResponse = await sut.handle()
 
     expect(httpResponse).toEqual(mockUserWithoutPasswordData)
   })
 
-  test('return 401 with user unauthenticated if email provided is invalid', async ({ expect }) => {
+  test('return an unauthorized exception if email provided is invalid', async ({ expect }) => {
     const { sut } = makeSut()
 
     stub(mockUserService, 'getFullInfoByUserLogged').rejects(new UnauthorizedException())
-    const httpResponse = sut.getFullInfoByUserLogged()
+    const httpResponse = sut.handle()
 
     expect(httpResponse).rejects.toEqual(new UnauthorizedException())
   })
 
-  test('return 422 with user not found if user was not found', async ({ expect }) => {
+  test('return an exception not found if user was not found', async ({ expect }) => {
     const { sut } = makeSut()
 
     stub(mockUserService, 'getFullInfoByUserLogged').rejects(new UserNotFoundException())
-    const httpResponse = sut.getFullInfoByUserLogged()
+    const httpResponse = sut.handle()
 
     expect(httpResponse).rejects.toEqual(new UserNotFoundException())
   })
 
-  test('return 500 if user getFullInfoByUserLogged throws', async ({ expect }) => {
+  test('return an exception if getFullInfoByUserLogged throws', async ({ expect }) => {
     const { sut } = makeSut()
 
     stub(mockUserService, 'getFullInfoByUserLogged').throws(new Error())
 
-    const httpResponse = sut.getFullInfoByUserLogged()
+    const httpResponse = sut.handle()
 
     expect(httpResponse).rejects.toEqual(new Error())
   })
