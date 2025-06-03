5c8bc032b196834a4c2bca82b328737fd432e727
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-06-02 21:54:02 -0300

    test: create favorite delete controller

--- backend/tests/functional/favorite-functional.spec.ts
+++ backend/tests/functional/favorite-functional.spec.ts
@@ -1,59 +1,10 @@
 import { test } from '@japa/runner'
 
 import { mockAllTables } from '#tests/__mocks__/db/mock-all'
-import { NilUUID } from '#tests/__utils__/NilUUID'
 
 test.group('Favorite Routes', async (group) => {
   group.tap((t) => {
     t.options.title = `it must ${t.options.title}`
-  })
-
-  test('/DELETE favorites/{uuid} - return 200 if video remove favorite on success', async ({
-    client,
-    expect,
-  }) => {
-    const { fakeVideo, fakeUser } = await mockAllTables()
-
-    const response = await client.delete(`favorites/${fakeVideo.uuid}`).loginAs(fakeUser)
-
-    expect(response.status()).toBe(200)
-    expect(response.body()).toBeTruthy()
-  })
-
-  test('/DELETE favorites/{uuid} - return 400 on remove favorite if video uuid invalid is provided', async ({
-    client,
-    expect,
-  }) => {
-    const { fakeUser } = await mockAllTables()
-
-    const response = await client.delete(`favorites/any_uuid`).loginAs(fakeUser)
-
-    expect(response.status()).toBe(400)
-    expect(response.body()).toEqual([
-      { field: 'uuid', message: 'The uuid field must be a valid UUID' },
-    ])
-  })
-
-  test('/DELETE favorites/{uuid} - return 404 on remove favorite if video not exists', async ({
-    client,
-    expect,
-  }) => {
-    const { fakeUser } = await mockAllTables()
-
-    const response = await client.delete(`favorites/${NilUUID}`).loginAs(fakeUser)
-
-    expect(response.status()).toBe(404)
-    expect(response.body().code).toEqual('E_VIDEO_NOT_FOUND')
-  })
-
-  test('/DELETE favorites/{uuid} - return 401 on remove favorite if user unauthorized', async ({
-    client,
-    expect,
-  }) => {
-    const response = await client.delete(`favorites/${NilUUID}`)
-
-    expect(response.status()).toBe(401)
-    expect(response.body().code).toBe('E_UNAUTHORIZED_ACCESS')
   })
 
   test('/GET favorites - return 200 on find a list favorite videos by user logged', async ({
--- backend/tests/unit/controllers/favorite-controller.spec.ts
+++ backend/tests/unit/controllers/favorite-controller.spec.ts
@@ -28,51 +28,6 @@ test.group('FavoriteController', (group) => {
     t.options.title = `it must ${t.options.title}`
   })
 
-  test('return 400 if invalid video uuid is provided on remove', async ({ expect }) => {
-    const { sut, httpContext: context } = await makeSut()
-
-    stub(context.request, 'params').returns({
-      uuid: 'invalid_uuid',
-    })
-
-    await sut.removeFavorite(context)
-
-    expect(context.response.getBody()).toEqual([
-      {
-        field: 'uuid',
-        message: 'The uuid field must be a valid UUID',
-      },
-    ])
-  })
-
-  test('return 404 if a video not found on remove', async ({ expect }) => {
-    const { sut, httpContext } = await makeSut()
-    stub(mockFavoriteService, 'removeFavorite').rejects(new VideoNotFoundException())
-    stub(httpContext.request, 'params').returns({
-      uuid: NilUUID,
-    })
-
-    const httpResponse = sut.removeFavorite(httpContext)
-
-    expect(httpResponse).rejects.toEqual(new VideoNotFoundException())
-  })
-
-  test('return 200 if video was remove favorite on success', async ({ expect }) => {
-    const { sut, httpContext } = await makeSut()
-
-    const httpResponse = await sut.removeFavorite(httpContext)
-    expect(httpResponse).toBeTruthy()
-  })
-
-  test('return 500 if video remove favorite throws', async ({ expect }) => {
-    const { sut, httpContext } = await makeSut()
-
-    stub(mockFavoriteService, 'removeFavorite').throws(new Error())
-
-    const httpResponse = sut.removeFavorite(httpContext)
-
-    expect(httpResponse).rejects.toEqual(new Error())
-  })
   test('return 200 on find favorites by user logged', async ({ expect }) => {
     const { sut } = await makeSut()
 
